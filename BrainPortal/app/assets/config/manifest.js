96bfb4ef7620ba651467b8621f93fab9143b74d1
Pierre Rioux <pierre.rioux@mcgill.ca>
2026-09-05 14:57:46 -0400

    Adjusted some helpers

--- BrainPortal/app/assets/config/manifest.js
+++ BrainPortal/app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
+//= link_directory ../javascripts/cbrain/tasks .js
+//= link_directory ../javascripts/cbrain/userfiles .js
+//= link_directory ../javascripts/cbrain/bourreaux .js
+//= link_directory ../javascripts/cbrain/shared .js
 //= link_directory ../stylesheets .css
--- BrainPortal/config/initializers/session_store.rb
+++ BrainPortal/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store,
-  :key          => 'BrainPortal5_Session',
+  :key          => 'BrainPortal8_Session',
   :same_site    => :lax,
   :expire_after => 3.days
